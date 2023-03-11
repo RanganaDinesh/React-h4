a46af5a9bb1a46dcdefb51cfb6ed4a238f0da0f4
karthik1131 <rangana.dinesh09@gmail.com>
2023-03-11 19:57:21 +0530

    reacth7

--- src/Components/Navbar/Home.jsx
+++ src/Components/Navbar/Home.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1 className="title" style={{marginLeft:'750px',marginTop:'200px'}}>.........Home page.........</h1>
+      <h1 className="title" style={{marginLeft:'550px',marginTop:'200px'}}>.........Home page.........</h1>
     </div>
   )
 }
